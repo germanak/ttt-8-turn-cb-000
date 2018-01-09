d1715ce3617f6b641fa82aff9c4d4fcc0266e84c
Alec German <alec_german@yahoo.com>
2018-01-09 00:06:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,5 +30,10 @@ def turn(board)
   input = gets.strip()
 
   index = input_to_index(input)
-  valid_move?(board, index) ? move(board, index) : turn(board)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
